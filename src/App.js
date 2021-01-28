dc3355d51be7d96b24ba4c525e7adbd9e4726813
Jonathan Nuno <jnuno.423@gmail.com>
2021-01-28 18:43:45 -0500

    added final search box setting, css styling, and made page responsive

--- src/App.js
+++ src/App.js
@@ -23,18 +23,21 @@ class App extends Component {
 
   render() {
     const { monsters, searchField } = this.state;
-    const filteredMonsters = monsters.filter(monster => 
+    const filteredMonsters = monsters.filter(monster =>
       monster.name.toLowerCase().includes(searchField.toLowerCase())
-      );
+    );
 
     return (
       <div className="App">
+        <h1>Monsters Search</h1>
         <SearchBox
           placeholder='search monsters'
           handleChange={e => this.setState({ searchField: e.target.value })}
         />
         <CardList monsters={filteredMonsters} />
+
       </div>
+
     );
   }
 }
